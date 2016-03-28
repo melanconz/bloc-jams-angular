ce32b8ab676196b10c0e1833b34dc9c34a875f9f
Zachary Melancon <zmelan01@gmail.com>
2016-03-28 14:54:55 -0500

    add styles and angular

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,26 @@
 module.exports = function(grunt) {
 
     grunt.registerTask( 'default', [ 'clean', 'copy', 'hapi', 'watch'] );
+    
+    grunt.registerTask('fun', 'This task is for fun only', function() {
+        grunt.log.writeln('This the *fun* Grunt task');
+    });
+ 
+    grunt.registerTask('serious', 'This task is for serious stuff only', function() {
+        grunt.log.writeln('Wipe that smirk off your face; this is serious.');
+    });
 
     grunt.registerTask( 'build', [ 'clean', 'copy' ] );
 
+    grunt.registerTask('default', 'Testing out Grunt logging and task callbacks', ['watch']);
+    
     grunt.registerTask( 'run', [ 'hapi', 'watch' ]);
+    
+    grunt.initConfig({
+        watch: {
+            files: ['Gruntfile.js']
+        }
+    })
 
     grunt.initConfig({
 
@@ -69,6 +85,7 @@ module.exports = function(grunt) {
                 }
             }
         },
+        
 
         clean: ['./dist']
     });
