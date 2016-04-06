fe630ad7d297ee5e862d7fd08acccbbd01a86127
Zachary Melancon <zmelan01@gmail.com>
2016-04-05 21:12:59 -0500

    add factory service for fixtures

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,15 +1,9 @@
-var albumData = angular.copy(albumPicasso);
-
 (function() {
-    function AlbumCtrl() {
-      this.album = albumData;
-      this.songs = [];
-      for (var i; i < albumData.songs.length; i++) {
-          this.songs.push(albumData.songs);
-      }
+    function AlbumCtrl(Fixtures) {
+      this.albumData = Fixtures.getAlbum();
     }
-    
+
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', AlbumCtrl);
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
