4fd8a522349cb6b2d0ed72203b4996a2e65f7d18
Zachary Melancon <zmelan01@gmail.com>
2016-04-06 21:57:05 -0500

    refactor code

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,6 +2,10 @@
   function SongPlayer() {
     var SongPlayer = {};
 
+    /**
+    * @desc song from album
+    * @type {Object}
+    */
     var currentSong = null;
 
     /**
@@ -29,11 +33,25 @@
       currentSong = song;
     };
 
+    /**
+    * @function playSong
+    * @desc play audio file and makes song.playing equal to true in order to show pause
+    * @param {Object} song
+    */
+    var playSong = function(song) {
+      currentBuzzObject.play();
+      song.playing = true;
+    };
+
+    /**
+    * @function SongPLayer.play
+    * @desc plays song, prevents already playing song to be played again, and shows play
+    * @param {Object} song
+    */
     SongPlayer.play = function(song) {
       if (currentSong !== song) {
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong(song);
       } else if (currentSong === song) {
         if (currentBuzzObject.isPaused()) {
           currentBuzzObject.play();
@@ -41,6 +59,11 @@
       }
     };
 
+    /**
+    * @function SongPlayer.pause
+    * @desc if song is currently playing, pauses song
+    * @param {Object} song
+    */
     SongPlayer.pause = function(song) {
       currentBuzzObject.pause();
       song.playing = false;
